fcfa980c07fd49d4884e588f0267c84d2f3d2364
EzioW <2024eb01099@online.bits-pilani.ac.in>
2025-09-22 21:42:13 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,8 +3,8 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22182189"))
-API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
+API_ID = int(environ.get("API_ID", "23855925"))
+API_HASH = environ.get("API_HASH", "e8b7d3e84b40b28c3279c3b1da259449")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 OWNER = int(environ.get("OWNER", "5680454765"))
@@ -33,3 +33,4 @@ photoyt = 'https://tinypic.host/images/2025/03/18/YouTube-Logo.wine.png' #https:
 photocp = 'https://tinypic.host/images/2025/03/28/IMG_20250328_133126.jpg'
 photozip = 'https://envs.sh/cD_.jpg'
 # .....,.....,.......,...,.......,....., .....,.....,.......,...,.
+
